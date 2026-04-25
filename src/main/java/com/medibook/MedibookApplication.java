8c2310df67993bb91a82f4fe9f0f75a48722ce1d
mattperrymatt45-pixel <mattperrymatt45@gmail.com>
2026-04-25 16:59:04 +0530

    Fix JPA repository scanning

--- src/main/java/com/medibook/MedibookApplication.java
+++ src/main/java/com/medibook/MedibookApplication.java
@@ -3,8 +3,11 @@ package com.medibook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.medibook")
+@EnableJpaRepositories(basePackages = "com.medibook.repository")
+@EntityScan(basePackages = "com.medibook.model")
 public class MedibookApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MedibookApplication.class, args);
     }
